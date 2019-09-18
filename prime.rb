365a0a4fc915eb00b8707156c808875a76d60a84
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-09-18 00:42:11 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+def prime?(number)
+  start = 2 
+  
+  if number > 1
+   numberArray = (2..number-1).to_a
+   numberArray.none?() { |num| number % num == 0 }
+  else
+    false
+  end
+end
