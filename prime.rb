1154335f90e391abe65d28bf528334df5c0e8e37
alycia hall <amhall25@gmail.com>
2019-09-22 00:02:42 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(num)
+  n = 2
+  while n < num
+    if num % n == 0
+      return false
+    end
+    n += 1
+  end
+  if n>num
+    return false
+  else
+  true
+end
+end
