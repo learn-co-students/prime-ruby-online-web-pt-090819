d64abf85bfbb5a09017811b9c002ba4593d4267d
ALEXANDRA OLIVEROS <alexofc@gmail.com>
2019-09-23 15:08:29 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(num)
+  range = (2...num).to_a
+  if num < 2
+    false
+  else
+   range.each do |number|
+     if num % number == 0
+      return false
+     end  
+   end 
+   true 
+  end
+end
